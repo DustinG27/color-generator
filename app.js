bd8eb548c975f3e89caa3e311d2da0216c2a5ef1
dustin george <DustinG27@gmail.com>
2021-08-07 14:19:37 -0400

    background color changes to appropriate color

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ btn.addEventListener('click', function (){
     let random1 = Math.floor(Math.random() * 256) + 1;
     let random2 = Math.floor(Math.random() * 256) + 1;
     let random3 = Math.floor(Math.random() * 256) + 1;
-    let bgcolor = docutment.querySelector('body');
+    // let bgColor = document.querySelector('body');
+    let color = `rgb(${random1}, ${random2}, ${random3})`;
 
     const label = document.querySelector("h1");
-    label.innerText = `rgb(${random1}/${random2}/${random3})`;
+    label.innerText = color;
 
+    // label.setAttribute("style", "background-color: rgb(${random1}/${random2}/${random3});");
+    document.body.style.backgroundColor = color;
     label.appendChild(label);
-    bgcolor.appendChild("<style>body{background-color:rgb(${random1}/${random2}/${random3})}</style>");
-    // document.head.insertAdjacentHTML("beforeend", `<style>body{background:rgb(${random1}/${random2}/${random3})}</style>`);
 });
